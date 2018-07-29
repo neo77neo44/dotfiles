6967762be1be75a69012222d8a806130929268af
neo.cheng <neo.cheng@vertexcom.com>
2018-07-28 21:49:33 -0700

    add install .sh

---
+++ .install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get -y install open-vm-tools-desktop
+
+sudo apt-get -y install git
+
+sudo apt-get -y install gitk
+
+git clone https://github.com/neo77neo44/dotfiles
+
+sudo apt-get -y install zsh
+
+sudo apt-get -y install powerline
+
+sudo apt-get -y install fonts-powerline
+
+sudo apt-get -y install vim
+
+sudo apt-get -y install curl
+
+sudo curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo apt-get -y install tmux
